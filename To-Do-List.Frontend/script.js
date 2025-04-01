1b61327bf8342c5d41586334e728fe4286cda63a
Davi Luquini <davi262016@gmail.com>
2025-04-01 18:42:23 -0300

    adding improvements to frontend

--- To-Do-List.Frontend/script.js
+++ To-Do-List.Frontend/script.js
@@ -1,15 +1,30 @@
-const inputs = document.querySelectorAll('.task-input');
-const buttons = document.querySelectorAll('.done-button');
-
+const inputContainers = document.querySelectorAll('.input-container');
+const inputs = document.querySelectorAll('.task-input'); 
+const doneButtons = document.querySelectorAll('.done-button');
+const clearTasks = document.querySelector('#clear-tasks');
 
 inputs.forEach((input, index) => {
-    const button = buttons[index];  
+    const doneButton = doneButtons[index];
+    const inputContainer = inputContainers[index];
 
-    input.addEventListener('hover', () => {
-        button.style.display = 'block';  
+    inputContainer.addEventListener('mouseenter', () => {
+        if( input.value !== '') {
+            doneButton.style.display = 'block';  
+        }
     });
 
-    input.addEventListener('blur', () => {
-        button.style.display = 'none';  
+    inputContainer.addEventListener('mouseleave', () => {
+        doneButton.style.display = 'none';  
     });
+
+    doneButton.addEventListener('click', () => {
+        input.value = '';
+        doneButton.style.display = 'none';
+    })
+});
+
+clearTasks.addEventListener('click', () => {
+    inputs.forEach((input) => { 
+        input.value = '';
+    })
 });
